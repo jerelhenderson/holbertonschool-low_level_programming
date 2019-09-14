5ecf8780a58441878c4a2beac5a5f1d4fcc96035
Jerel <henderson.jerel@yahoo.com>
2019-09-14 17:07:34 +0000

    Fix files to match printf command

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,10 +9,10 @@
  */
 int main(void)
 {
-	puts("Size of a char: %zu byte(s)\n", sizeof(char));
-	puts("Size of a int: %zu bytes(s)\n", sizeof(int));
-	puts("Size of a long int: %zu byte(s)\n", sizeof(long int));
-	puts("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-	puts("Size of a float: %zu byte(s)\n", sizeof(float));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of a int: %zu bytes(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 	return (0);
 }
