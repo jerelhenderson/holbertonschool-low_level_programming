b8e525bab29eb961ddd1266de2d0e1c7d2eaac2b
Jerel <henderson.jerel@yahoo.com>
2019-09-16 11:35:54 +0000

    FINAL FIX, JESUS CHRIST

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+ #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 
@@ -10,9 +10,9 @@
  */
 int main(void)
 {
-	char korpar[] = "and that piece of is useful\" - Dora Korpar, 2015-10-19\n";
+	char str[] = "and that piece of is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, korpar, strlen(korpar));
+	write(2, str, strlen(str));
 
 	return (1);
 }
