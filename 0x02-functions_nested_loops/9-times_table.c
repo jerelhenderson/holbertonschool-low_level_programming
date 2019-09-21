80e95afc70c658905aa22f8b746106e7799baa58
Jerel <henderson.jerel@yahoo.com>
2019-09-21 08:30:09 +0000

    Add Task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,47 @@
+#include "holberton.h"
+
+
+/**
+ * main - print times table
+ *
+ * Description: Print a times table, using 9
+ * Return: Always 0.
+ */
+void times_table(void)
+{
+	int x = 0;
+	int y;
+	int z;
+
+	while (x < 10)
+	{
+		y = 0;
+		while (y < 10)
+		{
+			z = x * y;
+
+			if (y != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (z < 10 && y != 0)
+			{
+				_putchar(' ');
+				_putchar('0' + z);
+			}
+			else if (z > 9)
+			{
+				_putchar('0' + (z / 10));
+				_putchar('0' + (z % 10));
+			}
+			else
+			{
+				_putchar('0' + z);
+			}
+			y++;
+		}
+		x++;
+		_putchar('\n');
+	}
+}
