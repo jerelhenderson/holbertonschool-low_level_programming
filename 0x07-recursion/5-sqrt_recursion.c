c4dbf8b69094289d8ce48bcf4133f801f397284b
Jerel <henderson.jerel@yahoo.com>
2019-10-06 18:33:16 +0000

    Add two tasks

--- 0x07-recursion/5-sqrt_recursion.c
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -1,1 +1,32 @@
 #include "holberton.h"
+
+/**
+ * _sqrt_recursion - return square root
+ *
+ * @n: integer type
+ * Return: Return -1 if no square root, or
+ * square root
+ */
+int _sqrt_recursion(int n)
+{
+	return (help(n, 1));
+}
+
+
+/**
+ * help - help function
+ *
+ * @c: integer type
+ * @i: integer type
+ * Return: square root, or -1 if none
+ */
+int help(int c, int i)
+	int help(int n, int i)
+{
+	if (i * i == n)
+		return (i);
+	else if (i * i > n)
+		return (-1);
+	else
+		return (help(n, i + 1));
+}
--- 0x07-recursion/6-is_prime_number.c
+++ 0x07-recursion/6-is_prime_number.c
@@ -1,1 +1,34 @@
 #include "holberton.h"
+
+
+/**
+ * is_prime_number - returns 1
+ *
+ * @n: integer type
+ * @i: integer type
+ * Return: 1 if prime
+ */
+int help(int n, int i);
+int is_prime_number(int n)
+{
+	return (help(n, n / 2));
+}
+
+/**
+ * help - prime or not
+ *
+ * @n: integer type
+ * @i: integer type
+ * Return: prime or nah
+ */
+int help(int n, int i)
+{
+	if (i < 1)
+		return (0);
+	if (n % i == 0)
+		return (0);
+	if (i == 1)
+		return (1);
+	else
+		return (help(n, i - 1));
+}
