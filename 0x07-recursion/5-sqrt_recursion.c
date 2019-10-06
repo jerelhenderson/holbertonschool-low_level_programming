432a5ec685036b135f574c0bf6828c7f9ab28a5b
Jerel <henderson.jerel@yahoo.com>
2019-10-06 18:39:49 +0000

    Real fix

--- 0x07-recursion/5-sqrt_recursion.c
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -4,23 +4,21 @@
  * _sqrt_recursion - return square root
  *
  * @n: integer type
- * Return: Return -1 if no square root, or
- * square root
+ * Return: square root, or -1 if nah
  */
 int _sqrt_recursion(int n)
 {
 	return (help(n, 1));
 }
 
-
 /**
  * help - help function
  *
  * @c: integer type
  * @i: integer type
- * Return: square root, or -1 if none
+ * Return: square root, or -1 if nah
  */
-int help(int c, int i)
+int help(int n, int i)
 {
 	if (i * i == n)
 		return (i);
