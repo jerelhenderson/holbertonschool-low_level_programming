b378622bee938622804bf5e3292b83bb5ae5c1d1
jerelhenderson <259@holbertonschool.com>
2019-10-13 19:27:02 +0000

    Project 0x0B More Malloc, Free - October 12

---
+++ 0x0B-more_malloc_free/3-array_range.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+
+/**
+ * array_range - allocate memory for an array of integers
+ *
+ * @min: minimum integers
+ * @max: maximum integers
+ * Return: array
+ */
+int *array_range(int min, int max)
+{
+	int i = 0;
+	int num;
+	int *arr;
+
+	if (min > max)
+		return (NULL);
+
+	arr = malloc((max - min + 1) * sizeof(int));
+	if (arr == NULL)
+		return (NULL);
+    
+  num = min;
+  while (num <= max)
+  {
+		arr[i] = num;
+    i++;
+    num++;
+   } 
+	return (arr);
+}
