112d0cfeba980fbcb05e1dea01b6a37f54170bf6
Jerel <henderson.jerel@yahoo.com>
2019-10-14 12:03:46 +0000

    Debug

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
 /**
  * str_concat - concactenates two strings
  *
@@ -13,7 +14,7 @@ char *str_concat(char *s1, char *s2)
 {
 	char *space;
 	int idx1, idx2;
-	int cpy1, cpy2;
+	int x;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -22,25 +23,36 @@ char *str_concat(char *s1, char *s2)
 
 	idx1 = 0;
 	idx2 = 0;
-	while (s1[idx1])
+	while (s1[idx1] != '\0')
 		idx1++;
-	while (s2[idx2])
+	while (s2[idx2] != '\0')
 		idx2++;
 
-	space = malloc((idx1 * sizeof(*s1)) + (idx2 * sizeof(*s2)));
-	{
+	x = idx1 + idx2;
+	printf("%d\n", x);
+
+	space = malloc(16);
 	if (space == NULL)
 		return (NULL);
-	}
+	printf("First: %d Second: %d Allocated: %lu", idx1, idx2, sizeof(space));
 
-	cpy1 = 0;
-	cpy2 = 0;
-	while (cpy1 < idx1 + idx2)
+/**
+        idx1 = 0;
+	while (s1[idx1] != '\0')
 	{
-		if (cpy1 < idx1)
-			space[cpy1] = s1[cpy1];
-		else
-			space[cpy1] = s2[cpy2++];
+		s1[idx1] = space[idx1];
+		idx1++;
 	}
+	printf("Boo here: %d", s1[idx1]);
+
+	idx2 = 0;
+	while (s2[idx2] != '\0')
+	{
+	       s2[idx2] = space[idx1];
+	       idx2++;
+	       idx1++;
+	}
+**/
+	space[idx1] = '\0';
 	return (space);
 }
