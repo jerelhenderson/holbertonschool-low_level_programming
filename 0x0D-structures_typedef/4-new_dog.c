11f63f4cc48eb133fec6ae75247c00aea8125817
Jerel <henderson.jerel@yahoo.com>
2019-10-18 10:12:51 +0000

    Fix some stuff, Add string copy/string length

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -3,6 +3,47 @@
 
 
 /**
+ * _strlen - length of a string
+ *
+ * @s: char type
+ * Return: returns the string length
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+/**
+ * _strcpy - copy string
+ *
+ * @dest: destination
+ * @src: source
+ * Return: pointer to @dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int length = 0;
+	int i;
+
+	while (src[length] != '\0')
+		length++;
+
+	i = 0;
+
+	while (i < length)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[length] = '\0';
+	return (dest);
+}
+
+/**
  * new_dog - create new doggie
  *
  * @name: character
@@ -11,3 +52,39 @@
  * Return: pointer to 'new dog'
  */
 dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct dog *puppy;
+	char *name1;
+	char *owner1;
+
+	puppy = malloc(sizeof(dog_t));
+	if (NULL)
+	{
+		free(puppy);
+		return (NULL);
+	}
+	name1 = malloc(_strlen(name) + 1);
+	if (NULL)
+	{
+		free(name1);
+		free(puppy);
+		return (NULL);
+	}
+	owner1 = malloc(_strlen(owner) + 1);
+	if (NULL)
+	{
+		free(owner1);
+		free(name1);
+		free(puppy);
+		return (NULL);
+	}
+
+	_strcpy(name1, name);
+	_strcpy(owner1, owner);
+
+	puppy->name = name1;
+	puppy->owner = owner1;
+	puppy->age = age;
+
+	return (puppy);
+}
