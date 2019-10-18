b46806eec585352b2cd94ce9186fad848ad015c5
Jerel <henderson.jerel@yahoo.com>
2019-10-18 11:43:05 +0000

    Forgot to incorporate (nil) in function

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include "dog.h"
 
+
 /**
  * print_dog - print 'struct dog'
  *
@@ -12,8 +13,8 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		printf("Name: %s\n", d->name);
+		printf("Name: %s\n", d->name ? d->name : "(nil)");
 		printf("Age: %f\n", d->age);
-		printf("Owner: %s\n", d->owner);
+		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 	}
 }
