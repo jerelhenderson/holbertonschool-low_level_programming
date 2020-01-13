2db0a5f3b466a8ae9cf31c241973ca7659bf4cf3
Jerel <henderson.jerel@yahoo.com>
2020-01-13 17:33:35 +0000

    Revert to initial code

--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -12,10 +12,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *temp;
 	unsigned long int index;
 
-	if (ht == NULL || key == NULL || key == '\0')
-		return (0);
+       	index = key_index((const unsigned char *)key, ht->size);
 
-	index = key_index((const unsigned char *)key, ht->size);
 	temp = ht->array[index];
 	while (ht->array[index] != NULL)
 	{
@@ -23,5 +21,5 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 			return (temp->value);
 		temp = temp->next;
 	}
-	return (0);
+	return NULL;
 }
