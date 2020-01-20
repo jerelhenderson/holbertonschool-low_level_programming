747561b2dd247dc460bfabaec68d69bed59c7138
jerelhenderson <259@holbertonschool.com>
2020-01-20 19:42:21 +0000

    Add fixes

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -1,30 +1,33 @@
 #include "hash_tables.h"
 
-/**
- * hash_table_print - print hash table
- *
- * @ht: hash table
- */
+/**                                                                                                                                                                      * hash_table_print - print hash table                                                                                                                                   *                                                                                                                                                                       * @ht: hash table                                                                                                                                                       */
 void hash_table_print(const hash_table_t *ht)
 {
-	hash_node_t *current;
-	unsigned long int idx;
-	unsigned long int itr;
+        hash_node_t *current;
+        unsigned long int idx;
+        unsigned long int count;
 
-	current = NULL;
-	idx = 0;
-	itr = 0;
+        current = NULL;
+        idx = 0;
+        count = 0;
 
-	if (ht == NULL)
-		return;
+        if (ht == NULL)
+                return;
 
-	printf("{");
-	while (idx < ht->size)
-		while (current != NULL)
-		{
-			printf(", ");
-			printf("'%s': '%s'", current->key, current->value);
-			current = current->next;
-		}
-	printf("}\n");
+        current = ht->array[idx];
+
+        printf("{");
+        while (idx < ht->size)
+        {
+                while (current != NULL)
+                {
+                        printf(", ");
+                        printf("'%s': '%s'", current->key, current->value);
+                        current = current->next;
+                        count++;
+                }
+                idx++;
+                current = ht->array[idx];
+        }
+        printf("}\n");
 }
