d10cb9e9fe8155fed54c61c508dfad4ffe33866f
jerelhenderson <259@holbertonschool.com>
2020-01-20 19:41:50 +0000

    Add fixes

--- 0x19-hash_tables/6-hash_table_delete.c
+++ 0x19-hash_tables/6-hash_table_delete.c
@@ -1,33 +1,36 @@
 #include "hash_tables.h"
 
-/**
- * hash_table_delete - delete a hash table
- *
- * @ht: hash table
- */
+/**                                                                                                                                                                      * hash_table_delete - delete a hash table                                                                                                                               *                                                                                                                                                                       * @ht: hash table                                                                                                                                                       */
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_node_t *current;
-	unsigned long int idx;
+        hash_node_t *current;
+        hash_node_t *next;
+        unsigned long int idx;
 
-	current = NULL;
-	idx = 0;
+        current = NULL;
+        next = NULL;
+        idx = 0;
 
-	if (ht == NULL)
-		return;
+        if (ht == NULL)
+                return;
 
-	while (idx < ht->size)
-	{
-		current = ht->array[idx];
-		while (current)
-		{
-			free(current->key);
-			free(current->value);
-			free(current);
+        current = ht->array[idx];
 
-			current = current->next;
-		}
-	}
-	free(ht->array);
-	free(ht);
+        while (idx < ht->size)
+        {
+                while (current)
+                {
+                        next = current;
+                        current = current->next;
+                        free(current->key);
+                        free(current->value);
+
+                        free(current);
+                        current = next;
+                }
+                current = ht->array[idx++];
+        }
+        idx++;
+        free(ht->array);
+        free(ht);
 }
