8e6c7a8ac7602f3276f39b501f894b78626707d2
Jerel Henderson <henderson.jerel@yahoo.com>
2020-03-09 19:36:55 +0000

    Correct files

--- 0x1C-binary_trees/12-binary_tree_leaves.c
+++ 0x1C-binary_trees/12-binary_tree_leaves.c
@@ -9,21 +9,16 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t left_leaf_num;
-	size_t right_leaf_num;
+	size_t leaf_num;
 
-	left_leaf_num = 0;
-	right_leaf_num = 0;
+	leaf_num = 0;
 
 	if (tree == NULL)
 		return (0);
 
-	left_leaf_num = left_leaf_num + binary_tree_leaves(tree->left);
-	right_leaf_num = right_leaf_num + binary_tree_leaves(tree->right);
+	leaf_num = leaf_num + binary_tree_leaves(tree->left);
+	leaf_num = leaf_num + binary_tree_leaves(tree->right);
 	if (tree->left == NULL && tree->right == NULL)
-	{
-		left_leaf_num++;
-		right_leaf_num++;
-	}
-	return ((left_leaf_num + right_leaf_num) / 2);
+		leaf_num++;
+	return (leaf_num);
 }
--- 0x1C-binary_trees/13-binary_tree_nodes.c
+++ 0x1C-binary_trees/13-binary_tree_nodes.c
@@ -9,21 +9,16 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	size_t left_node_num;
-	size_t right_node_num;
+	size_t node_num;
 
-	left_node_num = 0;
-	right_node_num = 0;
+	node_num = 0;
 
 	if (tree == NULL)
 		return (0);
 
-	left_node_num = left_node_num + binary_tree_nodes(tree->left);
-	right_node_num = right_node_num + binary_tree_nodes(tree->right);
+	node_num = node_num + binary_tree_nodes(tree->left);
+	node_num = node_num + binary_tree_nodes(tree->right);
 	if (tree->left == NULL && tree->right == NULL)
-	{
-		left_node_num++;
-		right_node_num++;
-	}
-	return (left_node_num + right_node_num);
+		node_num++;
+	return (node_num);
 }
