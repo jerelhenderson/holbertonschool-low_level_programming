ce1ed9af39f87e123856ad640aa6f0e7b4559727
Jerel <henderson.jerel@yahoo.com>
2020-11-25 10:50:36 +0000

    Change to `sizeof(char)`

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -16,12 +16,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	arr = malloc(nmemb * sizeof(*arr));
+	arr = malloc(nmemb * sizeof(char));
 	if (arr == NULL)
 		return (NULL);
 
 	for (i = 0; i < (nmemb * size); i++)
 		arr[i] = 0;
-
 	return (arr);
 }
