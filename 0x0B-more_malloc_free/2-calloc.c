c9c74e73e35d5e011f2ddaf48d2b186fd7a8ca59
Jerel <henderson.jerel@yahoo.com>
2020-11-26 16:15:02 +0000

    Add `size` to `malloc`

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -16,7 +16,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	arr = malloc(nmemb * sizeof(char));
+	arr = malloc((nmemb * size) * sizeof(*arr));
 	if (arr == NULL)
 		return (NULL);
 
