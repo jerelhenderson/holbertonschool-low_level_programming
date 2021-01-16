3a2a805f49ae5725442eb140e633685a3484ee5d
Jerel <henderson.jerel@yahoo.com>
2021-01-16 03:18:44 +0000

    Change which variables are freed if NULL

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -28,8 +28,8 @@ int **alloc_grid(int width, int height)
 		arr[i] = malloc(width * sizeof(int));
 		if (arr[i] == NULL)
 		{
-			for (; j > 1; j--)
-				free(arr[j]);
+			for (; i > 1; i--)
+				free(arr[i]);
 			free(arr);
 			return (NULL);
 		}
